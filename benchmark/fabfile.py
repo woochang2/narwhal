6c0a78398ca6688d85203ab65ced8d43cdd739d0
woochang2 <wcjeong@postech.ac.kr>
2022-12-16 14:35:01 +0900

    first commit

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -14,11 +14,11 @@ def local(ctx, debug=True):
     ''' Run benchmarks on localhost '''
     bench_params = {
         'faults': 0,
-        'nodes': 4,
+        'nodes': 64,
         'workers': 1,
-        'rate': 50_000,
-        'tx_size': 512,
-        'duration': 20,
+        'rate': 1_000_000,
+        'tx_size': 1_024,
+        'duration': 30,
     }
     node_params = {
         'header_size': 1_000,  # bytes
@@ -26,7 +26,7 @@ def local(ctx, debug=True):
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 10_000,  # ms
         'sync_retry_nodes': 3,  # number of nodes
-        'batch_size': 500_000,  # bytes
+        'batch_size': 102_400,  # bytes
         'max_batch_delay': 200  # ms
     }
     try:
